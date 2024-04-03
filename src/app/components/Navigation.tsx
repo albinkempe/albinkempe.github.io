8c1a6fb88d8fda4a6d3054db2bf7cd94b8ca16bd
albinwk <albinwk@kth.se>
2024-04-04 01:37:33 +0200

    added skeleton

---
+++ src/app/components/Navigation.tsx
@@ -0,0 +1,16 @@
+import TransitionLink from "./TransitionLink";
+
+const Navigation = () => {
+    return (
+        <nav className="w-full p-5 flex flex-row place-items-center justify-between">
+            <h1 className="text-3xl tracking-tight font-bold text-neutral-900">albinkempe</h1>
+            <div className="flex gap-5">
+                <TransitionLink href="/" label="Home" />
+                <TransitionLink href="/portfolio" label="Portfolio" />
+                <TransitionLink href="/about" label="About" />
+            </div>
+        </nav>
+    )
+}
+
+export default Navigation
---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,5 @@
+const PortfolioPage = () => {
+    return <h1 className="text-7xl font-extrabold text-center mt-10">Portfolio</h1>
+}
+
+export default PortfolioPage
