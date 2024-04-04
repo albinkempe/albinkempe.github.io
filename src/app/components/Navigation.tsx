6b655c594ca378f4932a604e5c3917e92c9345e9
albinwk <albinwk@kth.se>
2024-04-04 17:50:47 +0200

    Improved readability

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -3,7 +3,7 @@ import TransitionLink from "./TransitionLink";
 const Navigation = () => {
     return (
         <nav className="w-full p-5 flex flex-row place-items-center justify-between">
-            <h1 className="text-3xl tracking-tight font-bold text-neutral-900">albinkempe</h1>
+            <h1 className="text-3xl tracking-tight font-bold text-neutral-900">Albin Kempe</h1>
             <div className="flex gap-5">
                 <TransitionLink href="/" label="Home" />
                 <TransitionLink href="/portfolio" label="Portfolio" />
