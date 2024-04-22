27ffe8269e07450440f6b232c63ae8b6a0a9e320
albinwk <albinwk@kth.se>
2024-04-22 14:37:16 +0200

    temp

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,9 +2,9 @@ const AboutPage = () => {
     return (
         <div>
             <h1>About</h1>
-            <p className="about-p">I'm a UX/UI designer with a master's degree in computer science and a specialisation in interaction design.</p>
+            <p className="about-p">I am a UX/UI designer!</p>
             <p className="m-5">What I listen to when I work on this website</p>
-            <iframe className="spotify-embed" src="https://open.spotify.com/embed/track/3qhlB30KknSejmIvZZLjOD?utm_source=generator" width="100%" height="352" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            <iframe className="spotify-embed" src="https://open.spotify.com/embed/track/3qhlB30KknSejmIvZZLjOD?utm_source=generator" width="100%" height="352" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
         </div>
     )
 }
