22f48ca9b303009d962a4aa1590af84e841bc4dc
albinwk <albinwk@kth.se>
2024-04-22 14:32:53 +0200

    temporary website

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ const AboutPage = () => {
         <div>
             <h1>About</h1>
             <p className="about-p">I'm a UX/UI designer with a master's degree in computer science and a specialisation in interaction design.</p>
-            <p className="m-5">What I listen to when I work on this website 🎧</p>
+            <p className="m-5">What I listen to when I work on this website</p>
             <iframe className="spotify-embed" src="https://open.spotify.com/embed/track/3qhlB30KknSejmIvZZLjOD?utm_source=generator" width="100%" height="352" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
         </div>
     )
