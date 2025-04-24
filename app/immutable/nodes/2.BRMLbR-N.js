be26c2adae7996fd06c891a01a24950c2d544214
Albin Kempe <albin.kempe@outlook.com>
2025-04-24 18:35:21 +0200

    Updates

---
+++ app/immutable/nodes/2.BRMLbR-N.js
@@ -0,0 +1,1 @@
+import{t as a,a as e}from"../chunks/QIV4Hk93.js";import"../chunks/BnHYF_Ts.js";var p=a('<h1 class="text-3xl font-bold">Albin Kempe</h1>');function m(t){var o=p();e(t,o)}export{m as component};
