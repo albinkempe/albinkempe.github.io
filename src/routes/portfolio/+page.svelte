c594bf426e24c95d6dfcd9a8cc9bb9d6660d559e
Albin Kempe <albin.kempe@outlook.com>
2025-04-24 20:30:57 +0200

    basic webpage

---
+++ src/routes/portfolio/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { MoveLeft } from '@lucide/svelte';
+</script>
+
+<main class="flex h-screen flex-col items-center justify-center">
+	<h1 class="text-3xl font-bold">Portfolio</h1>
+	<div
+		class="m-8 flex w-9/10 flex-row items-center justify-center gap-16 rounded-md bg-neutral-900 p-4 lg:w-3/10 lg:gap-8"
+	>
+		<a
+			href="/"
+			class="rounded bg-neutral-900 p-2 text-white outline-2 outline-white hover:bg-blue-500"
+			><MoveLeft /></a
+		>
+	</div>
+</main>
--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,13 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		appDir: 'app', // Required as the default is _app
+		adapter: adapter()
+	},
+	prerender: {
+		default: true
+	}
 };
 
 export default config;
