6bb417e0e409540b2fb9702c49721564c796da91
Albin Kempe <albin.kempe@outlook.com>
2025-11-04 23:13:53 +0100

    cleaned up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main>
+    <h1>Albin Kempe</h1>
+</main>
+
+<style>
+    h1 {
+        color: #c6dff8;
+        font-family: monospace;
+        text-align: center;
+    }
+
+    main {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+
+    :global(body) {
+        margin: 0;
+        background-color: #000000;
+    }
+</style>
