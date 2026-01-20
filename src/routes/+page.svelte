19786982f6d31f53c601c2cfb8b959d496f5cc8a
Albin Kempe <albin.kempe@outlook.com>
2026-01-20 23:54:14 +0100

    Added sitemap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,38 @@
 <main>
-    <h1>Albin Kempe</h1>
+	<h1>Albin Kempe</h1>
+	<p>MSE Computer Science | KTH</p>
+
+    <a href="/sitemap">→ sitemap</a>
 </main>
 
 <style>
-    h1 {
+	:global(body) {
+		margin: 0;
+		background-color: #000000;
         color: #c6dff8;
         font-family: monospace;
-        text-align: center;
-    }
+	}
 
-    main {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-    }
+	main {
+		display: flex;
+		flex-direction: column;
+		justify-content: start;
+		align-items: center;
+		height: 100vh;
+	}
 
-    :global(body) {
-        margin: 0;
-        background-color: #000000;
+	h1 {
+		text-align: center;
+        margin-top: 30rem;
+	}
+
+	p {
+		text-align: center;
+	}
+
+    a {
+        margin-top: 20px;
+        text-decoration: none;
+        color: #c6dff8;
     }
 </style>
---
+++ src/routes/sitemap/+page.svelte
@@ -0,0 +1,49 @@
+<main>
+	<h1>sitemap</h1>
+
+    <ul>
+        <li><a href="/">home</a></li>
+        <li><a href="/sitemap">sitemap ←</a></li>
+        <li><a href="https://albinkempe.github.io/hsd/">hsd</a></li>
+        <li><a href="https://albinkempe.github.io/the-finals/">the-finals</a></li>
+        <li><a href="https://albinkempe.github.io/hsd-leaderboard/">hsd-leaderboard</a></li>
+        <li><a href="https://albinkempe.github.io/giza/">giza</a></li>
+    </ul>
+</main>
+
+<style>
+	:global(body) {
+		margin: 0;
+		background-color: #000000;
+        color: #c6dff8;
+        font-family: monospace;
+	}
+
+	main {
+		display: flex;
+		flex-direction: column;
+		justify-content: start;
+		align-items: center;
+		height: 100vh;
+	}
+
+	h1 {
+		text-align: center;
+        margin-top: 20rem;
+	}
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        margin: 10px 0;
+    }
+
+    a {
+        margin-top: 20px;
+        text-decoration: none;
+        color: #c6dff8;
+    }
+</style>
