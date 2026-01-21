31e9205548a804c4459299c925c33a6c8827800d
Albin Kempe <albin.kempe@outlook.com>
2026-01-21 23:45:49 +0100

    Fixed favicon fr

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,10 @@
-<script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
 
+<script lang="ts">
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="/favicon.png" />
 </svelte:head>
 
 {@render children()}
